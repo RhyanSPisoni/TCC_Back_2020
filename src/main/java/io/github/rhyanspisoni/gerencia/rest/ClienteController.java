9de02edb216981b69e2f4280bd3abad5d4515d5b
RhyanSPisoni <rhyanoficial14@gmail.com>
2020-11-30 18:14:04 -0300

    attCrossOrigin

--- src/main/java/io/github/rhyanspisoni/gerencia/rest/ClienteController.java
+++ src/main/java/io/github/rhyanspisoni/gerencia/rest/ClienteController.java
@@ -1,11 +1,13 @@
 package io.github.rhyanspisoni.gerencia.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/clientes")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ClienteController {
 }
--- src/main/java/io/github/rhyanspisoni/gerencia/rest/UsuarioController.java
+++ src/main/java/io/github/rhyanspisoni/gerencia/rest/UsuarioController.java
@@ -4,14 +4,17 @@ import io.github.rhyanspisoni.gerencia.model.entity.Usuario;
 import io.github.rhyanspisoni.gerencia.model.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/usuario")
 @RequiredArgsConstructor
 @CrossOrigin("*")
+
 public class UsuarioController {
     private final UsuarioRepository repository;
 
